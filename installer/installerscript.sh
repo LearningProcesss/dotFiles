1e696a9204c40c39831cfe61d43b2e9fe86beced
Mattia M <minerva.mattia.4j@gmail.com>
2021-12-16 20:30:59 +0100

    feat: brave

--- installer/installerscript.sh
+++ installer/installerscript.sh
@@ -40,3 +40,13 @@ echo '-> chrome'
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
 sudo apt install ./google-chrome-stable_current_amd64.deb
+
+echo '-> brave'
+
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+sudo apt update
+
+sudo apt install brave-browser -y
