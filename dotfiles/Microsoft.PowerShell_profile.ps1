7e263d50b86d94c23d6efa7e29e92a30a40723fc
Minerva Mattia <m.minerva@isydedom.isyde.com>
2022-04-26 09:29:36 +0200

    Add powershell script for WindowsTerminal

---
+++ dotfiles/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,8 @@
+oh-my-posh init pwsh | Invoke-Expression
+
+# Shows navigable menu of all options when hitting Tab
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+# Autocompletion for arrow keys
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
