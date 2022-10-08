6180b65f820743830ecccdd012091e18f8a4dd69
learningprocesss <learningprocesss@pop-os.localdomain>
2022-10-08 21:55:30 +0200

    add themes and tools

--- installer/installerscript.sh
+++ installer/installerscript.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+echo '-> nala'
+
+sudo apt install nala -y
+
+echo '-> bat'
+
+cd ~/Downloads && curl https://github.com/sharkdp/bat/releases/download/v0.22.1/bat_0.22.1_amd64.deb --output bat_0.22.1_amd64.deb && dpkg bat_0.22.1_amd64.deb 
+
 echo '-> Zsh'
 
-sudo apt install zsh -y
+sudo apt install zsh -y && chsh -s $(which zsh)
+
+echo '-> you should logoff'
 
 echo '-> OhMyZsh'
 
@@ -16,6 +26,38 @@ echo '-> OhMyZsh - plugin zsh-syntax-highlighting'
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+echo '-> nerd font'
+
+sudo apt install fonts-firacode
+
+echo $'-> set VsCode "editor.fontFamily": "\'Fira Code\'", "editor.fontLigatures": true'
+
+# cd ~/Downloads && curl https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/FiraCode.zip --output FiraCode.zip 
+
+echo '-> antigen'
+
+curl -L git.io/antigen > antigen.zsh
+
+# source "/path-to-your/antigen.zsh"
+
+echo '-> spaceship'
+
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1 && ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+echo '-> set ZSH_THEME="spaceship" into .zshrc'
+
+echo '-> starship'
+
+curl -sS https://starship.rs/install.sh | sh
+
+echo '-> set eval "$(starship init zsh)" at the end of .zshrc'
+
+echo '-> powerlevel10k'
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+echo '-> ZSH_THEME="powerlevel10k/powerlevel10k" in ~/.zshrc'
+
 echo '-> httpie'
 
 sudo apt install httpie -y
@@ -23,6 +65,24 @@ sudo apt install httpie -y
 echo '-> jq'
 
 sudo apt install jq -y
+
+echo '-> exa'
+
+sudo apt install exa -y
+
+echo '-> cheat'
+
+cd ~/Downloads \
+  && wget https://github.com/cheat/cheat/releases/download/4.3.3/cheat-linux-amd64.gz \
+  && gunzip cheat-linux-amd64.gz \
+  && chmod +x cheat-linux-amd64 \
+  && sudo mv cheat-linux-amd64 /usr/local/bin/cheat
+
+echo '-> cheat.sh'
+
+curl -s https://cht.sh/:cht.sh | sudo tee /usr/local/bin/cht.sh && sudo chmod +x /usr/local/bin/cht.sh
+
+sudo apt install rlwrap -y
 
 echo '-> GithubCli'
 
@@ -55,4 +115,16 @@ echo '-> git delta plugin'
 
 curl 'https://github.com/dandavison/delta/releases/download/0.11.3/git-delta_0.11.3_amd64.deb'
 
-cd ~/Downloads && curl https://github.com/dandavison/delta/releases/download/0.11.3/git-delta_0.11.3_amd64.deb --output git-delta_0.11.3_amd64.deb
+cd ~/Downloads && curl https://github.com/dandavison/delta/releases/download/0.11.3/git-delta_0.11.3_amd64.deb --output git-delta_0.11.3_amd64.deb && dpkg git-delta_0.11.3_amd64.deb
+
+echo '-> brew'
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /home/learningprocesss/.profile && echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/learningprocesss/.profile && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+echo '-> fx'
+
+brew install fx
+
+echo '-> yq'
+
+brew install yq
